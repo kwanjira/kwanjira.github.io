7152a8ebd4b1a1d7b3fcc292cfa5f2398badd3df
kwanjira <princesskwann@gmail.com>
2014-12-22 23:39:17 +0700

    11

--- README.txt
+++ README.txt
@@ -1,19 +1,18 @@
-Helios by HTML5 UP
+Astral by HTML5 UP
 html5up.net | @n33co
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-This is Helios, a brand new site template for HTML5 UP. It's clean, modern, and designed
-to take advantage of larger (well, wider) displays while still being capable of gracefully
-scaling down to fit all manner of smaller ones.
-	
-Demo images* courtesy of Michael Domaradzki, an awesome photographer I met over at
-deviantART. Check out his portfolio here:
+First of its kind on H5U! Astral features a flat, minimalistic design, a fully animated 
+interface (with noscript fallbacks), and styling for all basic page elements (including 
+blockquotes, tables and lists). Enjoy!
 
-http://md.photomerchant.net/
+Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
+you can use for pretty much whatever.
 
-(* = Not included! Only meant for use with my own on-site demo, so please do NOT download
-and/or use any of Michaels's work without his explicit permission!)
+(* = Not included)
+
+Feedback, bug reports, and comments are not only welcome, but strongly encouraged :)
 
 AJ
 n33.co @n33co dribbble.com/n33
@@ -22,17 +21,13 @@ n33.co @n33co dribbble.com/n33
 Credits:
 
 	Demo Images:
-		Michael Domaradzki (md.photomerchant.net)
+		Unsplash (unsplash.com)
 		
 	Icons:
 		Font Awesome (fortawesome.github.com/Font-Awesome)
-
+		
 	Other:
 		jQuery (jquery.com)
-		html5shiv.js (@afarkas @jdalton @jon_neal @rem)
-		CSS3 Pie (css3pie.com)
+		html5shiv.js (@afarkas @jdalton @jon_neal @rem)v
 		background-size polyfill (github.com/louisremi)
-		jquery.dropotron (n33.co)
-		jquery.scrolly (n33.co)
-		jquery.onvisible (n33.co)
 		skel (n33.co)
--- js/init.js
+++ js/init.js
@@ -1,304 +1,236 @@
 /*
-	Helios by HTML5 UP
+	Astral by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var settings = {
-		
-		// Header (homepage only)
-			header: {
-				fullScreen: true,
-				fadeIn: true,
-				fadeDelay: 500
-			},
+	/*********************************************************************************/
+	/* Settings                                                                      */
+	/*********************************************************************************/
 
-		// Carousels
-			carousels: {
-				speed: 4,
-				fadeIn: true,
-				fadeDelay: 250
-			},
-	
-	};
-	
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			'global':	{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 48 } },
-			'wide':		{ range: '-1680', href: 'css/style-wide.css', containers: 1200 },
-			'normal':	{ range: '-1280', href: 'css/style-normal.css', containers: '100%', grid: { gutters: 36 } },
-			'narrow':	{ range: '-960', href: 'css/style-narrow.css', grid: { gutters: 32 } },
-			'narrower': { range: '-840', href: 'css/style-narrower.css', containers: '100%!', grid: { collapse: true } },
-			'mobile':	{ range: '-736', href: 'css/style-mobile.css', grid: { gutters: 20 }, viewport: { scalable: false } }
-		},
-		plugins: {
-			layers: {
-				config: {
-					transformTest: function() { return skel.vars.isMobile; }
-				},
-				navPanel: {
-					hidden: true,
-					breakpoints: 'mobile',
-					position: 'top-left',
-					side: 'top',
-					width: '100%',
-					height: 250,
-					animation: 'pushY',
-					clickToHide: true,
-					swipeToHide: false,
-					html: '<div data-action="navList" data-args="nav"></div>',
-					orientation: 'vertical'
-				},
-				navButton: {
-					breakpoints: 'mobile',
-					position: 'top-center',
-					side: 'top',
-					width: 100,
-					height: 50,
-					html: '<div class="toggle" data-action="toggleLayer" data-args="navPanel"></div>'
-				}
+		var settings = {
+
+			// Speed to resize panel.
+				resizeSpeed: 600,
+			
+			// Speed to fade in/out.
+				fadeSpeed: 300,
+			
+			// Size factor.
+				sizeFactor: 11.5,		
+			
+			// Minimum point size.
+				sizeMin: 15,			
+			
+			// Maximum point size.
+				sizeMax: 20			
+
+		};
+
+	/*********************************************************************************/
+	/* skel                                                                          */
+	/*********************************************************************************/
+		
+		skel.init({
+			reset: 'full',
+			pollOnce: true,
+			breakpoints: {
+				'global':	{ range: '*', href: 'css/style.css' },
+				'desktop':	{ range: '737-', href: 'css/style-desktop.css', containers: 1200, grid: { gutters: 25 }, viewport: { width: 1080, scalable: false } },
+				'mobile':	{ range: '-736', href: 'css/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 15 }, viewport: { scalable: false } }
 			}
-		}
-	});
+		});
 
-	$(function() {
+	/*********************************************************************************/
+	/* Main                                                                          */
+	/*********************************************************************************/
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header =  $('#header');
-			
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-			
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-			
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
-
-		// Forms (IE<10).
-			var $form = $('form');
-			if ($form.length > 0) {
-				
-				$form.find('.form-button-submit')
-					.on('click', function() {
-						$(this).parents('form').submit();
-						return false;
-					});
+		var	$window = $(window);
 		
-				if (skel.vars.IEVersion < 10) {
-					$.fn.n33_formerize=function(){var _fakes=new Array(),_form = $(this);_form.find('input[type=text],textarea').each(function() { var e = $(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = $(this); var x = $($('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = $(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = $(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { $(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); $(this).find('select').val($('option:first').val()); $(this).find('input,textarea').each(function() { var e = $(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
-					$form.n33_formerize();
-				}
+		$window.on('load', function() {
 
+			var	$body = $('body'),
+				$main = $('#main'),
+				$panels = $main.find('.panel'),
+				$hbw = $('html,body,window'),
+				$footer = $('#footer'),
+				$wrapper = $('#wrapper'),
+				$nav = $('#nav'), $nav_links = $nav.find('a'),
+				$jumplinks = $('.jumplink'),
+				$form = $('form'),
+				panels = [],
+				activePanelId = null,
+				firstPanelId = null,
+				isLocked = false,
+				hash = window.location.hash.substring(1);
+		
+			if (skel.vars.isTouch) {
+				
+				settings.fadeSpeed = 0;
+				settings.resizeSpeed = 0;
+				$nav_links.find('span').remove();
+			
 			}
 
-		// Dropdowns.
-			$('#nav > ul').dropotron({ 
-				mode: 'fade',
-				speed: 350,
-				noOpenerFade: true,
-				alignment: 'center'
-			});
-
-		// Scrolly links.
-			$('.scrolly').scrolly();			
-
-		// Carousels.
-			$('.carousel').each(function() {
+			if (skel.isActive('desktop')) {
 				
-				var	$t = $(this),
-					$forward = $('<span class="forward"></span>'),
-					$backward = $('<span class="backward"></span>'),
-					$reel = $t.children('.reel'),
-					$items = $reel.children('article');
-				
-				var	pos = 0,
-					leftLimit,
-					rightLimit,
-					itemWidth,
-					reelWidth,
-					timerId;
-
-				// Items.
-					if (settings.carousels.fadeIn) {
-						
-						$items.addClass('loading');
-
-						$t.onVisible(function() {
-							var	timerId,
-								limit = $items.length - Math.ceil($window.width() / itemWidth);
-							
-							timerId = window.setInterval(function() {
-								var x = $items.filter('.loading'), xf = x.first();
-								
-								if (x.length <= limit) {
-									
-									window.clearInterval(timerId);
-									$items.removeClass('loading');
-									return;
-								
-								}
-								
-								if (skel.vars.IEVersion < 10) {
-									
-									xf.fadeTo(750, 1.0);
-									window.setTimeout(function() {
-										xf.removeClass('loading');
-									}, 50);
-								
-								}
-								else
-									xf.removeClass('loading');
-								
-							}, settings.carousels.fadeDelay);
-						}, 50);
-					}
-				
-				// Main.
-					$t._update = function() {
-						pos = 0;
-						rightLimit = (-1 * reelWidth) + $window.width();
-						leftLimit = 0;
-						$t._updatePos();
+				// Body.
+					$body._resize = function() {
+						var factor = ($window.width() * $window.height()) / (1440 * 900);
+						$body.css('font-size', Math.min(Math.max(Math.floor(factor * settings.sizeFactor), settings.sizeMin), settings.sizeMax) + 'pt');
+						$main.height(panels[activePanelId].outerHeight());
+						$body._reposition();
 					};
-				
-					if (skel.vars.IEVersion < 9)
-						$t._updatePos = function() { $reel.css('left', pos); };
-					else
-						$t._updatePos = function() { $reel.css('transform', 'translate(' + pos + 'px, 0)'); };
+
+					$body._reposition = function() {
+						if (skel.vars.isTouch && (window.orientation == 0 || window.orientation == 180))
+							$wrapper.css('padding-top', Math.max((($window.height() - (panels[activePanelId].outerHeight() + $footer.outerHeight())) / 2) - $nav.height(), 30) + 'px');
+						else
+							$wrapper.css('padding-top', ((($window.height() - panels[firstPanelId].height()) / 2) - $nav.height()) + 'px');
+					};
 					
-				// Forward.
-					$forward
-						.appendTo($t)
-						.hide()
-						.mouseenter(function(e) {
-							timerId = window.setInterval(function() {
-								pos -= settings.carousels.speed;
-
-								if (pos <= rightLimit)
-								{
-									window.clearInterval(timerId);
-									pos = rightLimit;
-								}
-								
-								$t._updatePos();
-							}, 10);
-						})
-						.mouseleave(function(e) {
-							window.clearInterval(timerId);
-						});
-				
-				// Backward.
-					$backward
-						.appendTo($t)
-						.hide()
-						.mouseenter(function(e) {
-							timerId = window.setInterval(function() {
-								pos += settings.carousels.speed;
-
-								if (pos >= leftLimit) {
-									
-									window.clearInterval(timerId);
-									pos = leftLimit;
-								
-								}
-								
-								$t._updatePos();
-							}, 10);
-						})
-						.mouseleave(function(e) {
-							window.clearInterval(timerId);
-						});
+				// Panels.
+					$panels.each(function(i) {
+						var t = $(this), id = t.attr('id');
 						
-				// Init.
-					$window.load(function() {
+						panels[id] = t;
+					
+						if (i == 0) {
+							
+							firstPanelId = id;
+							activePanelId = id;
+						
+						}
+						else
+							t.hide();
+							
+						t._activate = function(instant) {
+						
+							// Check lock state and determine whether we're already at the target.
+								if (isLocked
+								||	activePanelId == id)
+									return false;
 
-						reelWidth = $reel[0].scrollWidth;
+							// Lock.
+								isLocked = true;
+								
+							// Change nav link (if it exists).
+								$nav_links.removeClass('active');
+								$nav_links.filter('[href="#' + id + '"]').addClass('active');
+								
+							// Change hash.
+								if (i == 0)
+									window.location.hash = '#';
+								else
+									window.location.hash = '#' + id;
 
-						skel.change(function() {
+							// Add bottom padding.
+								var x = parseInt($wrapper.css('padding-top')) +
+										panels[id].outerHeight() +
+										$nav.outerHeight() +
+										$footer.outerHeight();
+							
+								if (x > $window.height())
+									$wrapper.addClass('tall');
+								else
+									$wrapper.removeClass('tall');
+										
+							// Fade out active panel.
+								$footer.fadeTo(settings.fadeSpeed, 0.0001);
+								panels[activePanelId].fadeOut(instant ? 0 : settings.fadeSpeed, function() {
 				
-							if (skel.vars.isTouch) {
-								
-								$reel
-									.css('overflow-y', 'hidden')
-									.css('overflow-x', 'scroll')
-									.scrollLeft(0);
-								$forward.hide();
-								$backward.hide();
+									// Set new active.
+										activePanelId = id;
+
+										// Force scroll to top.
+											$hbw.animate({
+												scrollTop: 0
+											}, settings.resizeSpeed, 'swing');
+
+										// Reposition.
+											$body._reposition();
+											
+										// Resize main to height of new panel.
+											$main.animate({
+												height: panels[activePanelId].outerHeight()
+											}, instant ? 0 : settings.resizeSpeed, 'swing', function() {
+											
+												// Fade in new active panel.
+													$footer.fadeTo(instant ? 0 : settings.fadeSpeed, 1.0);
+													panels[activePanelId].fadeIn(instant ? 0 : settings.fadeSpeed, function() {
+														
+														// Unlock.
+															isLocked = false;
+
+													});
+											});
+										
+								});
+						
+						};
+						
+					});
+
+				// Nav + Jumplinks.
+					$nav_links.add($jumplinks).click(function(e) {
+						var t = $(this), href = t.attr('href'), id;
+					
+						if (href.substring(0,1) == '#') {
 							
-							}
-							else {
-								
-								$reel
-									.css('overflow', 'visible')
-									.scrollLeft(0);
-								$forward.show();
-								$backward.show();
+							e.preventDefault();
+							e.stopPropagation();
+
+							id = href.substring(1);
 							
-							}
-
-							$t._update();
-						});
-
-						$window.resize(function() {
-							reelWidth = $reel[0].scrollWidth;
-							$t._update();
-						}).trigger('resize');
-
+							if (id in panels)
+								panels[id]._activate();
+						
+						}
+					
 					});
 				
-			});
-		
-		// Header.
-			if ($body.hasClass('homepage')) {
-				
-				if (settings.header.fullScreen) {
-					
-					$window.bind('resize.helios', function() {
-						window.setTimeout(function() {
-							var s = $header.children('.inner');
-							var sh = s.outerHeight(), hh = $window.height(), h = Math.ceil((hh - sh) / 2) + 1;
-
-							$header
-								.css('padding-top', h)
-								.css('padding-bottom', h);
-						}, 0);
-					}).trigger('resize');
-				
-				}
-
-				if (settings.header.fadeIn) {
-
-					$.n33_preloadImage = function(url, onload) { var $img = $('<img />'), _IEVersion = (navigator.userAgent.match(/MSIE ([0-9]+)\./) ? parseInt(RegExp.$1) : 99); $img.attr('src', url); if ($img.get(0).complete || _IEVersion < 9) (onload)(); else $img.load(onload); };
-					
-					$('<div class="overlay" />').appendTo($header);
-					
+				// Window.
 					$window
-						.load(function() {
-							var imageURL = $header.css('background-image').replace(/"/g,"").replace(/url\(|\)$/ig, "");
-
-							$.n33_preloadImage(imageURL, function() {
-								
-								if (skel.vars.IEVersion < 10)
-									$header.children('.overlay').fadeOut(2000);
-								else
-									window.setTimeout(function() {
-										$header.addClass('ready');
-									}, settings.header.fadeDelay);
-							
-							});
+						.resize(function() {
+						
+							if (!isLocked)
+								$body._resize();
+						
 						});
+						
+					if (skel.vars.IEVersion < 9)
+						$window
+							.on('resize', function() {
+								$wrapper.css('min-height', $window.height());
+							});
+
+				// Forms (IE<10).
+					if ($form.length > 0) {
+						
+						if (skel.vars.IEVersion < 10) {
+							$.fn.n33_formerize=function(){var _fakes=new Array(),_form = $(this);_form.find('input[type=text],textarea').each(function() { var e = $(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = $(this); var x = $($('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = $(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = $(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { $(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); $(this).find('select').val($('option:first').val()); $(this).find('input,textarea').each(function() { var e = $(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
+							$form.n33_formerize();
+						}
+
+					}
+
+				// CSS polyfills (IE<9).
+					if (skel.vars.IEVersion < 9)
+						$(':last-child').addClass('last-child');
+
+				// Init.
+					$window
+						.trigger('resize');
+
+					if (hash && hash in panels)
+						panels[hash]._activate(true);
+
+					$wrapper.fadeTo(400, 1.0);
 				
-				}
-
 			}
-
-	});
+					
+		});
 
 })(jQuery);
