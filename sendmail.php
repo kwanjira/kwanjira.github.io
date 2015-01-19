955bdb7bee633a5db78a819c77c714fc0ba0c251
kwanjira <princesskwann@gmail.com>
2015-01-20 01:20:38 +0700

    ด

--- sendmail.php
+++ sendmail.php
@@ -1,25 +1,11 @@
-<?php
-
-// ชื่อไฟล์ sendmail.php เพื่อรับค่าจาก form 
-
-$MailTo = $_POST['MailTo'] ;
-$MailFrom = $_POST['MailFrom'] ;
-$MailSubject = $_POST['MailSubject'] ;
-$MailMessage = $_POST['MailMessage'] ;
-
-$Headers = "MIME-Version: 1.0\r\n" ;
-$Headers .= "Content-type: text/html; charset=windows-874\r\n" ;
-// ส่งข้อความเป็นภาษาไทย ใช้ "windows-874"
-$Headers .= "From: ".$MailFrom." <".$MailFrom.">\r\n" ;
-$Headers .= "Reply-to: ".$MailFrom." <".$MailFrom.">\r\n" ;
-$Headers .= "X-Priority: 3\r\n" ;
-$Headers .= "X-Mailer: PHP mailer\r\n" ;
-
-if(mail($MailTo, $MailSubject , $MailMessage, $Headers, $MailFrom))
-{
-echo "Send Mail True" ; //ส่งเรียบร้อย
-}else{
-echo "Send Mail False" ; //ไม่สามารถส่งเมล์ได้
-}
-
+<?php 
+$ToEmail = 'princesskwann@gmail.com'; 
+$EmailSubject = 'ติดต่อห้องพัก'; 
+$mailheader = "From: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["name"]."<br>"; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"]."<br>"; 
+$MESSAGE_BODY .= "Comment: ".nl2br($_POST["comment"])."<br>"; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
 ?>
