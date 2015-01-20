ac87e798607339cc98ef003ad6b7ddbcaba67fb5
kwanjira <princesskwann@gmail.com>
2015-01-20 18:11:15 +0700

    อ

---
+++ phpSendMailContactForm.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>ติดต่อบ้านพัก</title>
+</head>
+<body>
+<?php 
+$ToEmail = 'princesskwann@gmail.com'; 
+$EmailSubject = 'Site contact form '; 
+$mailheader = "From: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Reply-To: ".$_POST["email"]."\r\n"; 
+$mailheader .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+$MESSAGE_BODY = "Name: ".$_POST["name"]."<br>"; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"]."<br>"; 
+$MESSAGE_BODY .= "Comment: ".nl2br($_POST["comment"])."<br>"; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Failure"); 
+?>
+</body>
+</html>
